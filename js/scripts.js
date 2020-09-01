2e08e2562707b74287086e43ed90327c60835161
huyct1996 <39883328+huyct1996@users.noreply.github.com>
2020-09-01 10:30:33 +0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -57,14 +57,14 @@ $(document).ready(function(){
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 768,
                 settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 375,
                 settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
